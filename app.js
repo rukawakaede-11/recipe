676410fd0430aed83886aa710cea431f662536f3
rukawakaede-11 <rukawakede11@gmail.com>
2024-12-10 02:18:02 +0900

    Updated app.js

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.get('/', (req, res) => {
   res.redirect('/auth/login');
 });
 
-const PORT = 3000;
+const PORT = 8888;
 app.listen(PORT, () => {
   console.log(`서버가 ${PORT}번 포트에서 실행 중입니다.`);
 });
